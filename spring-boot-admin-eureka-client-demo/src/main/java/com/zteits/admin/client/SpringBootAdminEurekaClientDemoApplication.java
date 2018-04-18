916e4d623fbaad61faf22b9581a8f50b2b6f1d32
jingjiqiang <jingjq@zte-its.com>
2018-04-18 10:26:07 +0800

    尝试采用eureka实现

---
+++ spring-boot-admin-eureka-client-demo/src/main/java/com/zteits/admin/client/SpringBootAdminEurekaClientDemoApplication.java
@@ -0,0 +1,12 @@
+package com.zteits.admin.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminEurekaClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminEurekaClientDemoApplication.class, args);
+	}
+}
---
+++ spring-boot-admin-eureka-server-demo/src/main/java/com/zteits/admin/server/SpringBootAdminSimpleServerDemoApplication.java
@@ -0,0 +1,24 @@
+package com.zteits.admin.server;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+public class SpringBootAdminSimpleServerDemoApplication {
+
+    private static final Logger logger = LoggerFactory
+            .getLogger(SpringBootAdminSimpleServerDemoApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminSimpleServerDemoApplication.class,
+                args);
+        logger.info("Application is in place!");
+    }
+}
